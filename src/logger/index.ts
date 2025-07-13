dd0b6f858fb26a7e1cb1f6a1a7aa5198715137fa
Nicholas Konar <nkonar@protonmail.com>
2025-07-13 11:36:11 -0400

    pino pretty print

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,6 +1,23 @@
 import pino from 'pino'
 
-export default pino({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+const devOpts = {
+  level: 'debug',
   timestamp: pino.stdTimeFunctions.isoTime,
-})
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'SYS:standard',
+      ignore: 'pid,hostname',
+      singleLine: false,
+    },
+  },
+}
+
+const prodOpts = {
+  level: 'info',
+  timestamp: pino.stdTimeFunctions.isoTime,
+}
+
+export default pino(process.env.NODE_ENV === 'production' ? prodOpts : devOpts)
+
